8da08e8a428e06fe01b9978807e5a01170860b7c
mustafavoltron <72883122+mustafavoltron@users.noreply.github.com>
2021-01-30 15:12:52 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,20 @@ basic.forever(function () {
     degrees = input.compassHeading()
     if (degrees < 45) {
         basic.showString("N")
+    } else if (degrees == 45) {
+        basic.showString("NE")
     } else if (degrees < 135) {
         basic.showString("E")
+    } else if (degrees == 135) {
+        basic.showString("SE")
     } else if (degrees < 225) {
         basic.showString("S")
+    } else if (degrees == 225) {
+        basic.showString("SW")
     } else if (degrees < 315) {
         basic.showString("W")
+    } else if (degrees == 315) {
+        basic.showString("NW")
     } else {
         basic.showString("N")
     }
